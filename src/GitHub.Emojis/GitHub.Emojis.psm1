a6da71b46ad70dfd61f8359eff4c2a4c4a7d1232
Marius <Marius.Storhaug@microsoft.com>
2022-02-05 14:24:30 +0100

    Added Emojis

--- src/GitHub.Emojis/GitHub.Emojis.psm1
+++ src/GitHub.Emojis/GitHub.Emojis.psm1
@@ -1,1 +1,34 @@
 ﻿# GitHub.Emojis
+
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER Token
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+https://docs.github.com/en/rest/reference/emojis#get-emojis
+#>
+function Get-GitHubEmojis {
+    [CmdletBinding()]
+    param (
+        $Destination
+    )
+
+    $Response = Invoke-GitHubAPI -Method Get -APIEndpoint emojis
+
+    if (Test-Path -Path $Destination) {
+        $Response.PSobject.Properties | ForEach-Object -Parallel {
+            Invoke-WebRequest -Uri $_.Value -OutFile "$using:Destination/$($_.Name).png"
+        }
+    }
+
+    return $Response
+}
