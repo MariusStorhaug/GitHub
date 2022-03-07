289374149f4aede1980face37f1a85751d5e243b
Marius Storhaug <marstor@hotmail.com>
2022-03-07 21:31:25 +0100

    Test full deployment

--- scripts/Publish-Module.ps1
+++ scripts/Publish-Module.ps1
@@ -7,16 +7,16 @@ param (
     [string] $APIKey
 )
 
-$Info = Invoke-Expression (Get-Content .\src\$ModuleName\$ModuleName.psd1 -Raw)
-foreach ($Module in $Info.RequiredModules) {
-    Write-Verbose "Install module: $($Module.ModuleName)"
-    try {
-        Install-Module -Name $Module.ModuleName -Force -Verbose
-    } catch {
-        Write-Warning 'Unable to install required module.'
-        Write-Warning $_.Exception.Message
-    }
-}
+# $Info = Invoke-Expression (Get-Content .\src\$ModuleName\$ModuleName.psd1 -Raw)
+# foreach ($Module in $Info.RequiredModules) {
+#     Write-Verbose "Install module: $($Module.ModuleName)"
+#     try {
+#         Install-Module -Name $Module.ModuleName -Force -Verbose
+#     } catch {
+#         Write-Warning 'Unable to install required module.'
+#         Write-Warning $_.Exception.Message
+#     }
+# }
 
 .\scripts\Set-ModuleVersion.ps1 -ModuleName $ModuleName -Verbose
 Publish-Module -Path "src/$ModuleName" -NuGetApiKey $APIKey -Verbose
